777997ea66b5f084e08066de5ddf45dced58098c
Stênio Oliveira <stenioti.dev@gmail.com>
2024-01-09 18:31:36 -0300

    first commit

---
+++ controller/controller.go
@@ -0,0 +1,18 @@
+package controller
+
+import (
+	"net/http"
+	"rest-api/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+var albums = []models.Album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sara Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func GetAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
---
+++ router/router.go
@@ -0,0 +1,14 @@
+package router
+
+import (
+	"rest-api/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+func StartRouter() {
+
+	router := gin.Default()
+	router.GET("/albums", controller.GetAlbums)
+	router.Run("localhost:8080")
+}
